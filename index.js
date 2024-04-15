031b2dfc4dddfc3d18fe713f70e1ae74abc09a63
Fiza Irfan <159923486+FizaIrfan3016@users.noreply.github.com>
2024-04-15 09:40:22 -0700

    Currency convertor code.

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+const currencyRates = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 8.76,
+    CYN: 7.24,
+    INR: 74.57,
+    JPV: 153,
+    PKR: 280,
+};
+let useranswers = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Enter from currency",
+        choices: ["USD", "EUR", "GBP", "CYN", "INR", "JPV", "PKR"]
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "Enter to currency",
+        choices: ["USD", "EUR", "GBP", "CYN", "INR", "JPV", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number"
+    }
+]);
+let fromAmount = currencyRates[useranswers.from];
+let toAmount = currencyRates[useranswers.to];
+let amount = useranswers.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(`Your converted amount is ${convertedAmount}`);
